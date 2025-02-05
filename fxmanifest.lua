c257c695143492545fb7c7f0dd201e808eaf5daf
Tabby Dev <88893827+Tabby-Labs@users.noreply.github.com>
2025-02-05 09:22:24 +0800

    Update manifest file and up version to 1.0.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-ui_page 'ui/index.html'
+author 'Tabby'
+description 'Simple Clean Notification UI'
+version '1.0.2'
 
 client_script 'client.lua'
 
+ui_page 'ui/index.html'
 files {
     'ui/*.html',
     'ui/*.css',
